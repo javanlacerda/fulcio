12d66675fdd7dc0b8c0774a23d8983b3aa6524bc
Javan lacerda <javanlacerda@google.com>
2024-05-21 20:58:22 +0000

    update to use templates lib
    Signed-off-by: Javan lacerda <javanlacerda@google.com>

--- pkg/poc/go.mod
+++ pkg/poc/go.mod
@@ -2,4 +2,7 @@ module poc2
 
 go 1.22.2
 
-require gopkg.in/yaml.v2 v2.4.0
+require (
+	gopkg.in/yaml.v2 v2.4.0
+	gopkg.in/yaml.v3 v3.0.1
+)
--- pkg/poc/pkg/reader.go
+++ pkg/poc/pkg/reader.go
@@ -16,64 +16,59 @@
 package main
 
 import (
+	"bytes"
+	"encoding/json"
 	"fmt"
+	"html/template"
+	"log"
 	"os"
 
-	"gopkg.in/yaml.v2"
+	"gopkg.in/yaml.v3"
 )
 
-type PreExtensions struct {
-	Fields []string
-
-	Issuer []string // OID 1.3.6.1.4.1.57264.1.8 and 1.3.6.1.4.1.57264.1.1 (Deprecated)
-
-	GithubWorkflowTrigger []string `yaml:"github-workflow-trigger"` // OID 1.3.6.1.4.1.57264.1.2
-
-	GithubWorkflowSHA []string `yaml:"github-workflow-sha"` // OID 1.3.6.1.4.1.57264.1.3
-
-	GithubWorkflowName []string `yaml:"github-workflow-name"` // OID 1.3.6.1.4.1.57264.1.4
-
-	GithubWorkflowRepository []string `yaml:"github-workflow-repository"` // OID 1.3.6.1.4.1.57264.1.5
-
-	GithubWorkflowRef []string `yaml:"github-workflow-ref"` // 1.3.6.1.4.1.57264.1.6
-
-	BuildSignerURI []string `yaml:"build-signer-uri"` // 1.3.6.1.4.1.57264.1.9
-
-	BuildSignerDigest []string `yaml:"build-signer-digest"` // 1.3.6.1.4.1.57264.1.10
-
-	RunnerEnvironment []string `yaml:"runner-environment"` // 1.3.6.1.4.1.57264.1.11
-
-	SourceRepositoryURI []string `yaml:"source-repository-uri"` // 1.3.6.1.4.1.57264.1.12
-
-	SourceRepositoryDigest []string `yaml:"source-repository-digest"` // 1.3.6.1.4.1.57264.1.13
-
-	SourceRepositoryRef []string `yaml:"source-repository-ref"` // 1.3.6.1.4.1.57264.1.14
-
-	SourceRepositoryIdentifier []string `yaml:"source-repository-identifier"` // 1.3.6.1.4.1.57264.1.15
-
-	SourceRepositoryOwnerURI []string `yaml:"source-repository-owner-uri"` // 1.3.6.1.4.1.57264.1.16
-
-	SourceRepositoryOwnerIdentifier []string `yaml:"source-repository-owner-identifier"` // 1.3.6.1.4.1.57264.1.17
-
-	BuildConfigURI []string `yaml:"build-config-uri"` // 1.3.6.1.4.1.57264.1.18
-
-	BuildConfigDigest []string `yaml:"build-config-digest"` // 1.3.6.1.4.1.57264.1.19
-
-	BuildTrigger []string `yaml:"build-trigger"` // 1.3.6.1.4.1.57264.1.20
-
-	RunInvocationURI []string `yaml:"run-invocation-uri"` // 1.3.6.1.4.1.57264.1.21
-
-	SourceRepositoryVisibilityAtSigning []string `yaml:"source-repository-visibility-at-signing"` // 1.3.6.1.4.1.57264.1.22
+type Extensions struct {
+	Issuer                              string // OID 1.3.6.1.4.1.57264.1.8 and 1.3.6.1.4.1.57264.1.1 (Deprecated)
+	GithubWorkflowTrigger               string `yaml:"github-workflow-trigger"`                 // OID 1.3.6.1.4.1.57264.1.2
+	GithubWorkflowSHA                   string `yaml:"github-workflow-sha"`                     // OID 1.3.6.1.4.1.57264.1.3
+	GithubWorkflowName                  string `yaml:"github-workflow-name"`                    // OID 1.3.6.1.4.1.57264.1.4
+	GithubWorkflowRepository            string `yaml:"github-workflow-repository"`              // OID 1.3.6.1.4.1.57264.1.5
+	GithubWorkflowRef                   string `yaml:"github-workflow-ref"`                     // 1.3.6.1.4.1.57264.1.6
+	BuildSignerURI                      string `yaml:"build-signer-uri"`                        // 1.3.6.1.4.1.57264.1.9
+	BuildSignerDigest                   string `yaml:"build-signer-digest"`                     // 1.3.6.1.4.1.57264.1.10
+	RunnerEnvironment                   string `yaml:"runner-environment"`                      // 1.3.6.1.4.1.57264.1.11
+	SourceRepositoryURI                 string `yaml:"source-repository-uri"`                   // 1.3.6.1.4.1.57264.1.12
+	SourceRepositoryDigest              string `yaml:"source-repository-digest"`                // 1.3.6.1.4.1.57264.1.13
+	SourceRepositoryRef                 string `yaml:"source-repository-ref"`                   // 1.3.6.1.4.1.57264.1.14
+	SourceRepositoryIdentifier          string `yaml:"source-repository-identifier"`            // 1.3.6.1.4.1.57264.1.15
+	SourceRepositoryOwnerURI            string `yaml:"source-repository-owner-uri"`             // 1.3.6.1.4.1.57264.1.16
+	SourceRepositoryOwnerIdentifier     string `yaml:"source-repository-owner-identifier"`      // 1.3.6.1.4.1.57264.1.17
+	BuildConfigURI                      string `yaml:"build-config-uri"`                        // 1.3.6.1.4.1.57264.1.18
+	BuildConfigDigest                   string `yaml:"build-config-digest"`                     // 1.3.6.1.4.1.57264.1.19
+	BuildTrigger                        string `yaml:"build-trigger"`                           // 1.3.6.1.4.1.57264.1.20
+	RunInvocationURI                    string `yaml:"run-invocation-uri"`                      // 1.3.6.1.4.1.57264.1.21
+	SourceRepositoryVisibilityAtSigning string `yaml:"source-repository-visibility-at-signing"` // 1.3.6.1.4.1.57264.1.22
 }
 
-type PreYaml struct {
-	Providers map[string]PreExtensions
+type YamlLocal struct {
+	Providers map[string]Extensions
+}
+
+func ApplyTemplate(path string, data map[string]string) string {
+	var doc bytes.Buffer
+	t := template.New("")
+	p, err := t.Parse(path)
+	if err != nil {
+		panic(err)
+	}
+	p.Execute(&doc, data)
+	return doc.String()
 }
 
 func main() {
-	var obj PreYaml
+	var obj YamlLocal
 
 	yamlFile, err := os.ReadFile("pkg/providers.yaml")
+
 	if err != nil {
 		fmt.Printf("yamlFile.Get err #%v ", err)
 	}
@@ -82,6 +77,43 @@ func main() {
 		fmt.Printf("Unmarshal: %v", err)
 	}
 
-	fmt.Println(obj)
+	runData := map[string]string{
+		"repository":       "ossf/scorecard",
+		"run_id":           "123",
+		"run_attempt":      "345",
+		"job_workflow_sha": "1a2b3c",
+		"project_path":     "project/path",
+		"job_id":           "123",
+	}
 
+	for k, v := range obj.Providers {
+
+		extensions := Extensions{
+			Issuer:                              ApplyTemplate(v.Issuer, runData),
+			GithubWorkflowTrigger:               ApplyTemplate(v.GithubWorkflowTrigger, runData),
+			GithubWorkflowSHA:                   ApplyTemplate(v.GithubWorkflowSHA, runData),
+			GithubWorkflowName:                  ApplyTemplate(v.GithubWorkflowName, runData),
+			GithubWorkflowRepository:            ApplyTemplate(v.GithubWorkflowRepository, runData),
+			GithubWorkflowRef:                   ApplyTemplate(v.GithubWorkflowRef, runData),
+			BuildSignerURI:                      ApplyTemplate(v.BuildSignerURI, runData),
+			BuildConfigDigest:                   ApplyTemplate(v.BuildConfigDigest, runData),
+			RunnerEnvironment:                   ApplyTemplate(v.RunnerEnvironment, runData),
+			SourceRepositoryURI:                 ApplyTemplate(v.SourceRepositoryURI, runData),
+			SourceRepositoryDigest:              ApplyTemplate(v.SourceRepositoryDigest, runData),
+			SourceRepositoryRef:                 ApplyTemplate(v.SourceRepositoryRef, runData),
+			SourceRepositoryIdentifier:          ApplyTemplate(v.SourceRepositoryIdentifier, runData),
+			SourceRepositoryOwnerURI:            ApplyTemplate(v.SourceRepositoryOwnerURI, runData),
+			SourceRepositoryOwnerIdentifier:     ApplyTemplate(v.SourceRepositoryOwnerIdentifier, runData),
+			BuildConfigURI:                      ApplyTemplate(v.BuildConfigURI, runData),
+			BuildSignerDigest:                   ApplyTemplate(v.BuildSignerDigest, runData),
+			BuildTrigger:                        ApplyTemplate(v.BuildTrigger, runData),
+			RunInvocationURI:                    ApplyTemplate(v.RunInvocationURI, runData),
+			SourceRepositoryVisibilityAtSigning: ApplyTemplate(v.SourceRepositoryVisibilityAtSigning, runData),
+		}
+		obj.Providers[k] = extensions
+	}
+	var prettyJSON bytes.Buffer
+	inrec, _ := json.Marshal(obj)
+	json.Indent(&prettyJSON, inrec, "", "\t")
+	log.Println(prettyJSON.String())
 }
